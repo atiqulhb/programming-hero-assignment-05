7712c7477d7c15af721ccd24aff33cbc20d819eb
Atiqul Hoque Bhuiyan <atiqulbhuiyan@google.com>
2026-09-14 19:17:30 +0600

    navbar added

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,36 @@
+import { NavLink } from 'react-router-dom'
+import Logo from '../assets/logo-text.png'
+
+const links = [
+  { label: "Home", path: "/" },
+  { label: "Technologies", path: "/technologies" },
+  { label: "Project", path: "/project" },
+  { label: "About", path: "/about" },
+  { label: "Contact", path: "/contact" }
+]
+
+export default function NavBar() {
+  return (
+    <nav className='w-[90vw] h-20 px-7.5 py-5 m-auto flex justify-between items-center text-sm font-medium leading-5'>
+      <NavLink key="landing-page" to="/">
+        <img src={Logo}/>
+      </NavLink>
+      <ul className='flex justify-between items-center gap-5 '>
+        {links.map((link) => (
+          <NavLink
+            key={link.path}
+            to={link.path}
+            end={link.path === "/"}
+            className={({ isActive }) => isActive ? "text-btn-text" : ""}
+            >
+              {link.label}
+          </NavLink>
+        ))}
+      </ul>
+      <div className='flex justify-between items-center gap-4'>
+        <NavLink key="/signin" to="/signin" className={({ isActive }) => isActive ? "text-btn-text" : ""}>Sign In</NavLink>
+        <NavLink  key="/signup" to="/signup" className='px-5 py-2.5 rounded-full bg-btn-bg text-white'>Sign Up</NavLink>
+      </div>
+    </nav>
+  )
+}
